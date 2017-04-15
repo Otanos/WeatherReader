e3045e4676c11b4e0d40e64153c9c04922b15489
Kuba-PC\Otanos <jakubnowacki17@gmail.com>
2017-04-15 16:11:29 +0200

    SqlSettings fix, added static method

--- WeatherReader/Settings/SqlSettings.cs
+++ WeatherReader/Settings/SqlSettings.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,14 @@ namespace WeatherReader.Settings
     public class SqlSettings
     {
         public static string dataBase = Properties.Resources.Database;
+
+        private static string _serverName = Properties.Resources.Server;
+
+        private static string _connectionString = string.Format("Server={0};Database={1};Trusted_Connection=True;", _serverName, dataBase);
+
+        private static SqlConnection _connection = new SqlConnection(_connectionString);
+
+        private static SqlCommand _command;
 
         public enum QueryType
         {
@@ -21,8 +31,56 @@ namespace WeatherReader.Settings
             DropTable
         }
 
+        public static void OpenConnection()
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Closed)
+                {
+                    _connection.Open();
+                }
+            }
+            catch(Exception ex)
+            {
+                System.Windows.Forms.DialogResult result;
 
+                result = System.Windows.Forms.MessageBox.Show(
+                    ex.Message, 
+                    "Connection failed", 
+                    System.Windows.Forms.MessageBoxButtons.RetryCancel, 
+                    System.Windows.Forms.MessageBoxIcon.Error
+                    );
+                
+                if(result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    OpenConnection();
+                }                
+            }
+            
+        }
 
+        public static void CloseConnection()
+        {
+            try
+            {
+                _connection.Close();
+            }
+            catch(Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        public static void ExecuteCommandQuery(string query)
+        {
+            OpenConnection();
+
+            _command = new SqlCommand(query, _connection);
+            _command.CommandType = CommandType.Text;
+            _command.ExecuteNonQuery();
+
+            CloseConnection();
+        }
 
     }
 }
